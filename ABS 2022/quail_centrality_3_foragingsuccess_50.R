26b3f534a9885e415dcd1f9a6f2c5da84e6eadd3
Sanjay Prasher <prashesy@mail.uc.edu>
2022-07-04 14:43:20 -0400

    completed script for getting forager (agents other than producer) success metrics

---
+++ ABS 2022/quail_centrality_3_foragingsuccess_50.R
@@ -0,0 +1,90 @@
+# Script for getting metrics of foraging success for agents other than the producer
+
+library(dplyr)
+library(stringr)
+
+group.sizes = c(3, 6, 10, 15, 20)
+
+n.loops = max(group.sizes)
+pb = txtProgressBar(min=0, max = n.loops, style=3)
+start.time = Sys.time()
+for(i in group.sizes) {
+  
+  if(i==3){
+    setwd("C:/Users/sanja/Documents/Sanjay's stuff/QuailCentralityABM/R analyses/quail_centrality_3/ABS 2022/group_size_3")
+  } else if(i==6) {
+    setwd("C:/Users/sanja/Documents/Sanjay's stuff/QuailCentralityABM/R analyses/quail_centrality_3/ABS 2022/group_size_6")
+  } else if(i==10) {
+    setwd("C:/Users/sanja/Documents/Sanjay's stuff/QuailCentralityABM/R analyses/quail_centrality_3/ABS 2022/group_size_10")
+  } else if(i==15) {
+    setwd("C:/Users/sanja/Documents/Sanjay's stuff/QuailCentralityABM/R analyses/quail_centrality_3/ABS 2022/group_size_15")
+  } else if(i==20) {
+    setwd("C:/Users/sanja/Documents/Sanjay's stuff/QuailCentralityABM/R analyses/quail_centrality_3/ABS 2022/group_size_20")
+  }
+  
+  q.data = read.csv("q_data_split.csv", header=T)[, -c(1, 14:393)] #exclude proximity columns (goes up to 393 for group size of 20)
+  
+  #FOR EACH PARAMETER COMBO, I NEED:
+  # a count of how many time steps each agent or any agent other than the producer successfully foraged 
+    #although the number of time steps might not change as much across parameter combos if more individuals are foraging in the same time step
+  # average energy level of foragers other than the producer at the end of the model (ticks = 300)
+  
+  
+  #for count of time steps - group by run number, get count, then get average count for each parameter combo, which you can plot
+  chr.to.find = as.character(1:(i-1)) #want to find all foragers except the producer (agent 0)
+  chr.to.find = paste(chr.to.find, collapse = "|")
+  
+  #data.to.count = q.data[grep(chr.to.find, q.data$current.succ.foragers),] #this gives all rows with time steps in which any forager other than the producer successfully foraged - the producer also foraged in many of these time steps
+  
+  for.success = q.data %>% 
+    filter(rownames(q.data) %in% grep(chr.to.find, q.data$current.succ.foragers)) %>% #this gives all rows with time steps in which any forager other than the producer successfully foraged - the producer also foraged in many of these time steps
+    group_by(run.num, memory, attention, preference, approach.food) %>% 
+    summarize(n.timesteps = n()) 
+    
+  # length(unique(for.success$run.num))
+  # for.success is missing some run numbers because the ones where foragers other than the producer never foraged were removed when subsetting 
+  # NEED TO FILL IN THE DATA FRAME TO INCLUDE ALL RUN NUMBERS
+    
+  data.to.add = q.data[!(q.data$run.num %in% for.success$run.num) & q.data$ticks == 0, c(1, 3:6)] #subset of q.data containing one row from each run number that does not appear in for.success and columns 1, and 3:6 (run.num, memory, attention, preference, and approach.food)
+  #nrow(data.to.add)
+  data.to.add$n.timesteps = rep(0, nrow(data.to.add))
+  
+  for.success = rbind(for.success, data.to.add)
+  #length(unique(for.success$run.num))
+  rm(data.to.add)
+  for.success = for.success[order(for.success$run.num),]
+  
+  
+  # for average energy level of foragers, remove all rows where ticks is not 300; separate forager energy levels into separate columns; pivot longer so the energy levels are stacked in the same column; calculate average per run number or combo number
+  q.data = q.data[q.data$ticks==300,]
+  #nrow(q.data)
+  q.data$energy.list = gsub("[[]", "", q.data$energy.list)
+  q.data$energy.list = gsub("[]]", "", q.data$energy.list)
+  energy.split = str_split_fixed(q.data$energy.list, " ", i) #split data in energy.list column into i columns
+  
+  colnames(energy.split) = LETTERS[1:i] #set column names - unique letter for each agent (A = producer)
+  
+  q.data = cbind(q.data, energy.split)
+  
+  q.data = q.data %>% 
+    pivot_longer(LETTERS[1:i], #pivot_longer is the same as melt in the reshape2 package
+                 names_to = "agent", values_to = "energy")
+  
+  #now calculate average energy level for each run number
+  q.data$energy = as.numeric(q.data$energy)
+  q.data = q.data[q.data$agent != "A",] %>% #IGNORING ROWS FOR PRODUCER BECAUSE I AM INTERESTED IN THE AVERAGE ENERGY LEVEL OF THE OTHER FORAGERS
+    group_by(run.num) %>%
+    summarize(mean.energy = mean(energy), var.energy = var(energy)) #I checked that the mean values were accurate by using mutate() here instead of summarize
+  
+  # add average energy level of foragers per run number to for.success data frame and write it to a csv file
+  for.success = merge(for.success, q.data, by = "run.num")# I am merging to be safe, but the data should be in the correct order in both data frames, so I could just do for.success$mean.energy = q.data$mean.energy
+  write.csv(for.success, "for_success.csv")
+  
+  setTxtProgressBar(pb,i)#update progress bar
+  
+}#END OF LOOP
+end.time = Sys.time()
+run.time = end.time - start.time
+run.time #ran in 32 seconds for 5 runs per combo
+
+#for_success.csv can now be used for plotting, etc.
